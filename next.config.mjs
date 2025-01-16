cfd76d313463233cd48af1ddf9e3c1a27effbf3f
Samarth Kamble <kamblesamarth04@gmail.com>
2025-01-16 20:56:24 +0530

    Add new remote pattern for images in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -9,26 +9,11 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { hostname: "adamant-antelope-943.convex.cloud", protocol: "https" },
+      { hostname: "flippant-ermine-890.convex.cloud", protocol: "https" },
     ],
   },
   experimental: {
     missingSuspenseWithCSRBailout: false,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lovely-flamingo-139.convex.cloud",
-      },
-      {
-        protocol: "https",
-        hostname: "shiny-pelican-899.convex.cloud",
-      },
-      {
-        protocol: "https",
-        hostname: "img.clerk.com",
-      },
-    ],
   },
 };
 
